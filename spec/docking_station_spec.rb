53b032c876389f668d3235811f43cb258580a6f6
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-08 15:23:40 +0000

    chapter 12 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,36 @@
 require 'docking_station'
 describe DockingStation do 
-    it { is_expected.to respond_to :release_bike}
-    it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it {is_expected.to respond_to :release_bike}
+    describe '#release_bike' do 
+        it 'releases a bike' do 
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq bike 
+        end 
+        describe '#release_bike' do
+            it 'raises an error when there are no bikes available' do
+           
+              expect { subject.release_bike }.to raise_error 'No bikes available'
+            end
+        end 
+    end 
+    
+    #it 'releases working bikes' do
+    #bike = subject.release_bike
+    #expect(bike).to be_working
+    #end
+    
+    it 'returns docked bikes' do 
+        bike = Bike.new
+    expect(subject.dock(bike)).to eq bike 
+    end
+    
+    it 'should show the docked bikes' do 
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bike).to eq bike 
+    end 
+
+
 end 
-end 
+#it { is_expected.to respond_to :working?}
