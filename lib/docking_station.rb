2b706bee0301cd0b9722dbc859133a7759c2768c
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-08 16:24:32 +0000

    Completed step 14 - updating to allow capacity of 20 by default

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,18 @@
 require_relative 'bike'
 class DockingStation
+  def initialize(capacity = 20)
+    @contents = []
+    @capacity = capacity
+  end
+
     def release_bike
-        fail "No bikes available" unless @bike 
-        @bike 
-end 
+        fail "No bikes available" if @contents.empty?
+        @contents.pop
+end
 def dock(bike)
-    fail "docking station full" if @bike 
-@bike = bike 
-end 
-attr_reader :bike
-end 
+    fail "docking station full" if @contents.length >= @capacity
+    @contents << bike
+    bike
+end
+attr_reader :contents
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,42 +1,38 @@
 require 'docking_station'
-describe DockingStation do 
+describe DockingStation do
     it {is_expected.to respond_to :release_bike}
-    describe '#release_bike' do 
-        it 'releases a bike' do 
+    describe '#release_bike' do
+        it 'releases a bike' do
             bike = Bike.new
             subject.dock(bike)
-            expect(subject.release_bike).to eq bike 
-        end 
+            expect(subject.release_bike).to eq bike
+        end
         describe '#release_bike' do
             it 'raises an error when there are no bikes available' do
-           
+
               expect { subject.release_bike }.to raise_error 'No bikes available'
             end
-           
-        end 
 
-    end 
-    
-     it 'returns docked bikes' do 
-        bike = Bike.new
-    expect(subject.dock(bike)).to eq bike 
+        end
+
     end
-    
-    it 'should show the docked bikes' do 
+
+     it 'returns docked bikes' do
+        bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'should show the docked bikes' do
         bike = Bike.new
         subject.dock(bike)
-        expect(subject.bike).to eq bike 
-    end 
-    it 'raises an error when the docking station has reached its capacity' do 
-       subject.dock(Bike.new)
+        expect(subject.contents).to include bike
+    end
+
+    it 'raises an error when the docking station has reached its capacity' do
+       20.times { subject.dock(Bike.new) }
        expect { subject.dock Bike.new}.to raise_error 'docking station full'
-    end 
-
-       
-        
-    end 
-
-    
+    end
 
 
 
+    end
